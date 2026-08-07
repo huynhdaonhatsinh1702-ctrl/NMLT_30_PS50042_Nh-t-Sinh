8cac10fcd9f0158cba85de1b04c701b8debec0cd
huynhdaonhatsinh1702 <huynhdaonhatsinh1702@gmail.com>
2026-08-07 05:39:56 -0500

    Lab5: Hoan thanh Chuc nang 1 - Xay dung ham findMax tim max trong 3 so"

--- Lab5/Main.menu.Lab5.c
+++ Lab5/Main.menu.Lab5.c
@@ -42,3 +42,32 @@ int main(){
     
     return 0;
 }
+void menu(){
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHƯƠNG TRÌNH LAB 5               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tìm giá trị lớn nhất trong 3 số                |\n");
+    printf("| 2. Kiểm tra Năm nhuận                             |\n");
+    printf("| 3. Hoán vị 2 số (Sử dụng Con trỏ)                 |\n");
+    printf("| 4. Kiểm tra & Phân loại Tam giác                  |\n");
+    printf("| 5. Thoát chương trình                             |\n");
+    printf("+---------------------------------------------------+\n");
+}
+
+void chucNang1(){
+    int a,b,c;
+    printf("Nhap a,b,c: ");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("So lon nhat: %d\n",soLonNhat(a,b,c));
+}
+
+int soLonNhat(int a, int b, int c){
+    int lonNhat = a;
+    if(lonNhat<b){
+        lonNhat = b;
+    }else if (lonNhat < c)
+    {
+        lonNhat = c;
+    }
+    return lonNhat;
+}
